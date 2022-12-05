cc897471f8dc50c88c5b37c3baf3c10ae70cd7ca
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2022-12-05 22:40:16 +0530

    render trying

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API = axios.create({ baseURL: 'http://localhost:5000'})
-const API = axios.create({ baseURL: 'https://segment-fault.vercel.app/'})
+const API = axios.create({ baseURL: 'https://segmentfault.onrender.com'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
