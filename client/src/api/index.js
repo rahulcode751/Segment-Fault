1183c7f67d9e59d66c989ce94dd58ba4a027dada
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2022-12-05 22:03:49 +0530

    client vercel done

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000'})
+// const API = axios.create({ baseURL: 'http://localhost:5000'})
+const API = axios.create({ baseURL: 'https://segment-fault.vercel.app/'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
