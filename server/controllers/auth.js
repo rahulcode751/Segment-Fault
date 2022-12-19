5497dcad23ca6a54f43ef7f57020b47cb8ffe9f2
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2022-12-19 23:17:29 +0530

    host changes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,7 +11,6 @@ export const signup = async (req, res) => {
         if (existinguser) {
             return res.status(404).json({ message: "User already Exist." })
         }
-
         const hashedPassword = await bcrypt.hash(password, 12)
         const newUser = await users.create({ name, email, password: hashedPassword })
         const token = jwt.sign({ email: newUser.email, id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
