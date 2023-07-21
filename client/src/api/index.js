3de6daeca29105d92b4ccbc57d647c3bf1b3ae87
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2023-07-21 23:00:37 +0530

    done

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-// const API = axios.create({ baseURL: 'http://localhost:5000' })
+// const API = axios.create({ baseURL: "http://localhost:8080" })
 // const API = axios.create({ baseURL: 'https://segmentfault.onrender.com' })
 const API = axios.create({ baseURL: 'https://segmentfaultbackendmain.onrender.com' })
 
@@ -12,16 +12,34 @@ API.interceptors.request.use((req) => {
     return req;
 })
 
-export const logIn = (authData) => API.post('/user/login', authData);
-export const signUp = (authData) => API.post('/user/signup', authData);
+// export const logIn = (authData) => API.post('/user/login', authData);
+// export const signUp = (authData) => API.post('/user/signup', authData);
 
-export const postQuestion = (questionData) => API.post('/questions/Ask', questionData)
-export const getAllQuestions = () => API.get('/questions/get');
-export const deleteQuestion = (id) => API.delete(`/questions/delete/${id}`)
-export const voteQuestion = (id, value) => API.patch(`/questions/vote/${id}`, { value })
+// export const postQuestion = (questionData) => API.post('/questions/Ask', questionData)
+// export const getAllQuestions = () => API.get('/questions/get');
+// export const deleteQuestion = (id) => API.delete(`/questions/delete/${id}`)
+// export const voteQuestion = (id, value) => API.patch(`/questions/vote/${id}`, { value })
 
-export const postAnswer = (id, noOfAnswers, answerBody, userAnswered) => API.patch(`/answer/post/${id}`, { noOfAnswers, answerBody, userAnswered })
-export const deleteAnswer = (id, answerId, noOfAnswers) => API.patch(`/answer/delete/${id}`, { answerId, noOfAnswers })
+// export const postAnswer = (id, noOfAnswers, answerBody, userAnswered) => API.patch(`/answer/post/${id}`, { noOfAnswers, answerBody, userAnswered })
+// export const deleteAnswer = (id, answerId, noOfAnswers) => API.patch(`/answer/delete/${id}`, { answerId, noOfAnswers })
 
-export const getAllUsers = () => API.get('/user/getAllUsers');
-export const updateProfile = (id, updateData) => API.patch(`/user/update/${id}`, updateData)
+// export const getAllUsers = () => API.get('/user/getAllUsers');
+// export const updateProfile = (id, updateData) => API.patch(`/user/update/${id}`, updateData)
+export const logIn = (authData) => API.post("/user/login", authData);
+export const signUp = (authData) => API.post("/user/signup", authData);
+
+export const postQuestion = (questionData) =>
+    API.post("/questions/Ask", questionData);
+export const getAllQuestions = () => API.get("/questions/get");
+export const deleteQuestion = (id) => API.delete(`/questions/delete/${id}`);
+export const voteQuestion = (id, value) =>
+    API.patch(`/questions/vote/${id}`, { value });
+
+export const postAnswer = (id, noOfAnswers, answerBody, userAnswered) =>
+    API.patch(`/answer/post/${id}`, { noOfAnswers, answerBody, userAnswered });
+export const deleteAnswer = (id, answerId, noOfAnswers) =>
+    API.patch(`/answer/delete/${id}`, { answerId, noOfAnswers });
+
+export const getAllUsers = () => API.get("/user/getAllUsers");
+export const updateProfile = (id, updateData) =>
+    API.patch(`/user/update/${id}`, updateData);
