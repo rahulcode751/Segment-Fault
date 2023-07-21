12015945d4492e4b426fa5e14f9ff0bd3f2a4486
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2023-07-21 22:09:20 +0530

    temp resolve

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 // }
 // app.use(cors(corsOptions));
 
-//dotenv.config();
+// dotenv.config();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 const corsOrigin = {
